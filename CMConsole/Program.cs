3c233d34e067c1a66070a2951ebb36a9598ddf68
John Kipson <johnkipson@gmail.com>
2018-11-16 12:14:48 +0530

    initial commit

---
+++ CMConsole/Program.cs
@@ -0,0 +1,53 @@
+﻿using CompanyManagementDataLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMConsole
+{
+    class Program
+    {
+
+        DataLayer dataLayer = new DataLayer();
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+            program.GetAllProjects();
+            program.GetAllTechnologies();
+            program.GetEmployeeCountForProject(1);
+
+            Console.Read();  
+        }
+
+
+        public void GetAllProjects()
+        {
+            List<Project> projects = dataLayer.GetAllProjects();
+
+            foreach(Project project in projects)
+            {
+                Console.WriteLine("Project Name is : " + project.ProjectName);
+            }
+
+
+        }
+        public void GetAllTechnologies()
+        {
+            List<TechnologyMaster> technologies = dataLayer.GetAllTechnologies();
+            foreach (TechnologyMaster technology in technologies)
+            {
+                Console.WriteLine("Technology Name is : " + technology.TechnologyName);
+            }
+
+
+        }
+        public void GetEmployeeCountForProject(int projectID)
+        {
+            int employeeCount = dataLayer.GetEmployeeCountForProject(projectID);
+            Console.WriteLine("Employee Count" + employeeCount);
+
+        }
+    }
+}
---
+++ CompanyManagementDataLayer/DataLayer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompanyManagementDataLayer
+{
+    public class DataLayer
+    {
+
+        CMDataClassesDataContext dataContext = new CMDataClassesDataContext();
+
+        public List<Project> GetAllProjects()
+        {
+            List<Project> projects = (from project in dataContext.Projects
+                                      select project).ToList();
+            return projects;
+
+        }
+        public List<TechnologyMaster> GetAllTechnologies()
+        {
+            List<TechnologyMaster> technologies = (from technology in dataContext.TechnologyMasters
+                                                   select technology).ToList();
+            return technologies;
+        }
+        public int GetEmployeeCountForProject(int projectID)
+        {
+            int employeeCount = (from empProject in dataContext.EmployeeProjects
+                                   where empProject.ProjectId == projectID
+                                   select empProject).Count();
+           
+            return employeeCount;
+        }
+    }
+}
